25d80b55856b0baefdfdf20976e5239e2e09f786
floatint <34470431+floatint@users.noreply.github.com>
2018-02-17 19:48:14 +0300

    Update functions.cpp
    Fix

--- functions.cpp
+++ functions.cpp
@@ -15,7 +15,7 @@ int IsKeyPress(lua_State *L)
 		return 1;
 	};
 	unsigned short ret = GetAsyncKeyState(key);
-	if ((ret == 32768) || (ret == 1) || (ret == 32769))
+	if ((ret == 32768) /*|| (ret == 1)*/ || (ret == 32769))
 	{
 		lua_pushboolean(L, true);
 		return 1;
